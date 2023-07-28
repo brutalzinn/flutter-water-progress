34642b72507fcb38a3886d32ed6392590c0cdb70
brutalzinn <robertocpaes@gmail.com>
2023-07-27 23:07:57 -0300

    init repo

---
+++ Makefile
@@ -0,0 +1,2 @@
+test:
+	go run web-socket-tester/main.go
--- web-socket-tester/main.go
+++ web-socket-tester/main.go
@@ -1,37 +1,60 @@
-package websockettester
+package main
 
 import (
 	"log"
+	"time"
 
+	"github.com/go-co-op/gocron"
 	"github.com/gofiber/fiber"
+	"github.com/gofiber/websocket"
 )
 
 /*
 [BLOG.ROBERTOCPAES.DEV - HYPERFOCUS - PERSONAL DATA COMMENTARY - IGNORE]
-  Lets do some go horse power and use fiber as web socket server while i am waiting my new weight modules.
+
+	Lets do some go horse power and use fiber as web socket server while i am waiting my new weight modules.
+
 [BLOG.ROBERTOCPAES.DEV - HYPERFOCUS - PERSONAL DATA COMMENTARY - IGNORE]
 */
 
+type GoWebSocketTesterRequest struct {
+	Cron string `json:"cron"`
+}
+type GoWebsocketTesterResponse struct {
+	Type  string `json:"type"`
+	Value int    `json:"value"`
+}
+
 func main() {
 	//really like express. i like it.
-
+	counterMax := 100
 	app := fiber.New()
-
 	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
-		// Websocket logic
+		//lets start a new goroutine that can send a random increment message to the flutter app and we can plot the gauge.
 		for {
-			mtype, msg, err := c.ReadMessage()
+			s := gocron.NewScheduler(time.UTC)
+			var GoWebSocketTesterRequest GoWebSocketTesterRequest
+			err := c.ReadJSON(&GoWebSocketTesterRequest)
 			if err != nil {
 				break
 			}
-			log.Printf("Read: %s", msg)
-
-			err = c.WriteMessage(mtype, msg)
+			log.Printf("Received: %s", GoWebSocketTesterRequest)
+			counter := 0
+			s.CronWithSeconds(GoWebSocketTesterRequest.Cron).Do(func() {
+				c.WriteJSON(GoWebsocketTesterResponse{Value: counter, Type: "weight"})
+				counter++
+				log.Println("Running %s", time.Now())
+			})
+			c.WriteJSON(GoWebSocketTesterRequest)
 			if err != nil {
 				break
 			}
+			if counter >= counterMax {
+				counter = 0
+			}
+			s.StartBlocking()
 		}
-		log.Println("Error:", err)
+
 	}))
 
 	log.Fatal(app.Listen(":3000"))
